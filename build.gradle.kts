9cbfa61dd2c5c1e6b63125c24eef0cdc038fea2e
--replace-all <wfarat@gmail.com>
2024-10-24 21:54:59 +0200

    Add MariaDB client dependency and update .gitignore
    Included the MariaDB Java client library to facilitate database interactions. Also, updated .gitignore to exclude application.properties from version control.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.4.1")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
